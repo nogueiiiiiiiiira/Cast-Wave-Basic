7ab39fff76efedca2894c25ef18873d5d347d33d
Karen <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2025-05-05 08:34:21 -0300

    Update logout.php

--- paginas/logout.php
+++ paginas/logout.php
@@ -6,15 +6,6 @@ session_start();
 // verifica se o usuário está logado
 $_SESSION = array();
 
-// se você estiver usando cookies, limpe o cookie
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params(); // obtém os parâmetros do cookie
-    setcookie(session_name(), '', time() - 42000, // set o cookie para expirar no passado
-        $params["path"], $params["domain"], // define o caminho, domínio, seguro e httponly
-        $params["secure"], $params["httponly"] // define se o cookie é seguro e httponly
-    );
-}
-
 session_destroy(); // destrói a sessão
 
 header("Location: /cast-wave-basic/html/login.html"); // redireciona para a página de login
